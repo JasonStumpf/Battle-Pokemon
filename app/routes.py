8b90d91ddd35891c6ee8a83470069dd42b9c6a4a
Jason Stumpf <jstumpf518@yahoo.com>
2023-07-17 13:06:52 -0700

    New Commit

--- app/routes.py
+++ app/routes.py
@@ -89,3 +89,12 @@ def get_pokemon():
         'status' : 'ok',
         'pokemons' : poke_list
     }
+
+@app.get('/pokemondata/<int:id>')
+def get_pokemon(id):
+    pokemon = Pokemon.query.get(id)
+    single_list = [pokemon.to_dict()]
+    return {
+        'status': 'ok',
+        'pokemon': single_list
+    }
